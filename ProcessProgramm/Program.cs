c281ca1c7069faf74a834630c7a21004d79249c2
AlexandrTolstov <alextav_85@mail.ru>
2019-01-06 12:23:42 +0600

    Процессы

--- ProcessProgramm/Program.cs
+++ ProcessProgramm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace ProcessProgramm
     {
         static void Main(string[] args)
         {
+            foreach (Process process in Process.GetProcesses())
+            {
+                Console.WriteLine("ID: {0} Name: {1}", process.Id, process.ProcessName);
+            }
+
+            Process proc = Process.GetProcessesByName("devenv")[0];
+            Console.WriteLine("ID: {0}", proc.Id);
+
+            Console.WriteLine("___________________________________");
+            ProcessThreadCollection processThread = proc.Threads;
+            foreach (ProcessThread thread in processThread)
+            {
+                Console.WriteLine("Thread: {0} StartTime: {1}", thread.Id, thread.StartTime);
+            }
+            Console.WriteLine("___________________________________");
+            ProcessModuleCollection modules = proc.Modules;
+            foreach (ProcessModule module in modules)
+            {
+                Console.WriteLine("Name: {0} MemorySize: {1}", module.ModuleName, module.ModuleMemorySize);
+            }
+
+            //Process.Start("http://google.com");
+            //Process.Start("G://Price.php");
+            //Process.Start(@"C:\Program Files\Adobe\Adobe Photoshop CC 2017\Photoshop.exe");
+
         }
     }
 }
